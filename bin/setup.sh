f7350c17a0b68518f0b346acbc7510c2d0fe76ac
arguskay99 <>
2023-06-10 14:40:46 +0200

    create function for installing ansible in order to avoid installing it multiple times

--- bin/setup.sh
+++ bin/setup.sh
@@ -25,16 +25,27 @@ function failOnError() {
     fi
 }
 
+function ensurePackageIsInstalled() {
+    # source: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
+    local requiredPackage="${1}"
+    local isPackageInstalled
+    isPackageInstalled=$(dpkg-query -W --showformat='${Status}\n' "${requiredPackage}"|grep "install ok installed")
+    echoinfo "Checking for ${requiredPackage}: ${isPackageInstalled}"
+    if [ "" = "${isPackageInstalled}" ]; then
+      echoinfo "No ${requiredPackage}. Setting up ${requiredPackage}."
+      sudo apt update
+      failOnError ${?} "failed to update packages"
+      sudo apt --yes install "${requiredPackage}"
+      failOnError ${?} "failed to install ansible"
+      echok "installed ansible successfully"
+    fi
+}
+
+
+
 echoinfo "starting setup script"
 
-
-echoinfo "installing ansible"
-sudo apt update
-failOnError ${?} "failed to update apt"
-sudo apt install -y ansible
-failOnError ${?} "failed to install ansible"
-
-echok "done"
+ensurePackageIsInstalled ansible
 
 echoinfo "running ansible playbook.yml"
 ansible-playbook playbook.yml
